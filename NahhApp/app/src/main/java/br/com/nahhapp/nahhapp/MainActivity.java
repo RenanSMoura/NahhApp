3385287ba049084bf2bce27ae4edb10dc8911123
renansmoura <renansmoura@hotmail.com>
2016-11-10 21:56:51 -0200

    Lógica da segunda activity pronta

--- NahhApp/app/src/main/java/br/com/nahhapp/nahhapp/MainActivity.java
+++ NahhApp/app/src/main/java/br/com/nahhapp/nahhapp/MainActivity.java
@@ -3,12 +3,9 @@ package br.com.nahhapp.nahhapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,23 +26,11 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.botao);
 
-
-
         button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-//                String abc = "R.string.teste1";
-//                textoPadrao.setText(getString(R.string.teste2));
-//
-//                try{
-//                    JSONObject jsonObject = new JSONObject(json);
-//                    Log.i("JSON",jsonObject.getJSONObject("menssagens").getString("1"));
-//                }catch (Exception e){
-//                    e.printStackTrace();
-//                }
-
-                Intent intent = new Intent(MainActivity.this,MudaMensagnsActivity.class);
+                Intent intent = new Intent(MainActivity.this,MudaMensagensActivity.class);
                 startActivity(intent);
 
             }
---
+++ NahhApp/app/src/main/java/br/com/nahhapp/nahhapp/MudaMensagensActivity.java
@@ -0,0 +1,157 @@
+package br.com.nahhapp.nahhapp;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Random;
+
+public class MudaMensagensActivity extends AppCompatActivity {
+
+    ImageButton btnAvancar;
+    ImageButton btnVoltar;
+    ImageButton btnRandom;
+    TextView    mainText;
+    TextView    codeHash;
+    int         value;
+    RelativeLayout mudaMensagemActivity;
+    JSONObject jsonObject = null;
+    String json = "{\n"+
+            "  \"mensagens\": {\n"+
+            "    \"0\": \"Te amo\",\n"+
+            "    \"1\": \"leao\",\n"+
+            "    \"2\": \"cabeção\"\n"+
+            "  }\n"+
+            "}";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muda_mensagens);
+
+        btnAvancar = (ImageButton) findViewById(R.id.avancar);
+        btnVoltar  = (ImageButton) findViewById(R.id.voltar);
+        btnRandom  = (ImageButton) findViewById(R.id.random);
+        mainText   = (TextView) findViewById(R.id.main_text);
+        codeHash   = (TextView) findViewById(R.id.number_hash);
+
+        btnAvancar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                verifica(v);
+            }
+        });
+
+        btnVoltar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                verifica(v);
+            }
+        });
+
+        btnRandom.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                verifica(v);
+            }
+        });
+
+        try {
+            jsonObject = new JSONObject(json);
+            mainText.setText(jsonObject.getJSONObject("mensagens").getString(String.valueOf(0)));
+            codeHash.setText("#" + String.valueOf(1));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void verifica(View view){
+        switch (view.getId()){
+            case R.id.avancar:
+
+                if(value ==2){
+                    value = 2;
+                }else{
+                    value++;
+                    alterarTexto(value);
+                }
+
+                break;
+            case R.id.voltar:
+                if(value == 0){
+                    value = 0;
+                }else{
+                    value--;
+                    alterarTexto(value);
+                }
+
+                break;
+            case R.id.random:
+                alterarTexto(getRandomValue(value));
+
+                break;
+        }
+        alterarBackGround();
+    }
+
+    private void alterarTexto(int valor){
+        try{
+            mainText.setText(jsonObject.getJSONObject("mensagens").getString(String.valueOf(valor)));
+            codeHash.setText("#" + String.valueOf(valor));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private int getRandomValue(int valor){
+        int randomNumber = valor;
+        while (valor == randomNumber){
+            randomNumber = new Random().nextInt(100);
+        }
+        Log.v("Cor", "Numero do texto" + randomNumber);
+        value = randomNumber;
+        return randomNumber;
+    }
+    private void alterarBackGround(){
+        mudaMensagemActivity = (RelativeLayout) findViewById(R.id.activity_muda_mensagns);
+        String  jsonCores = "{\n" +
+                "  \"cores\": {\n" +
+                "    \"0\": \"#c7b4b4\",\n" +
+                "    \"1\": \"#096790\",\n" +
+                "    \"2\": \"#c72828\",\n" +
+                "    \"3\": \"#ffc125\",\n" +
+                "    \"4\": \"#1ca154\",\n" +
+                "    \"5\": \"#E80C7A\"\n" +
+                "  }\n" +
+                "}";
+
+        int randomNumber = new Random().nextInt(6);
+        Log.v("Cor", "Numero da cor" + randomNumber);
+        try {
+            JSONObject jsonObject = new JSONObject(jsonCores);
+            mudaMensagemActivity.setBackgroundColor(Color.parseColor(
+                    jsonObject.getJSONObject("cores").getString(
+                    String.valueOf(randomNumber))));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+}
